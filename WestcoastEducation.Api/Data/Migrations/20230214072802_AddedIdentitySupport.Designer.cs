1e2d9c9a6eabb8a63d38a0ba54335c05cb3372ea
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-02-14 08:31:38 +0100

    Ny migration för Identity

--- WestcoastEducation.Api/Data/Migrations/20230214072802_AddedIdentitySupport.Designer.cs
+++ WestcoastEducation.Api/Data/Migrations/20230214072802_AddedIdentitySupport.Designer.cs
@@ -2,17 +2,20 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WestcoastEducation.Api.Data;
 
 #nullable disable
 
-namespace WestcoastEducationRESTDel2.api.Data.Migrations
+namespace WestcoastEducationRESTDel3.api.Data.Migrations
 {
     [DbContext(typeof(WestcoastEducationContext))]
-    partial class WestcoastEducationContextModelSnapshot : ModelSnapshot
+    [Migration("20230214072802_AddedIdentitySupport")]
+    partial class AddedIdentitySupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.1");
